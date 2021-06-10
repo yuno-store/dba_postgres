34797cea0c8f4827469b29f03f357a756016cd86
Niyamaka <niyamaka@yuneta.io>
2021-06-10 09:42:27 +0200

    ficheros de log a W.log

--- dba_postgres/src/main.c
+++ dba_postgres/src/main.c
@@ -54,7 +54,7 @@ PRIVATE char variable_config[]= "\
         'daemon_log_handlers': {                                    \n\
             'to_file': {                                            \n\
                 'handler_type': 'file',                             \n\
-                'filename_mask': 'dba_postgres-MM-DD.log',          \n\
+                'filename_mask': 'dba_postgres-W.log',          \n\
                 'handler_options': 255                              \n\
             },                                                      \n\
             'to_udp': {                                             \n\
